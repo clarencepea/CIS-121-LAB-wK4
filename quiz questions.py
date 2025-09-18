792abdec38527b9ea0aec532c5b8b62accea9c9b
clarencepea <clarencepeah@gmail.com>
2025-09-18 12:31:25 -0500

    completed question 1-4

---
+++ quiz questions.py
@@ -0,0 +1,36 @@
+# clarence 
+'''
+#larger_num = int(input("enter the larger number: "))
+#smaller_num = int(input("enter the smaller number: "))
+
+#while larger_num > smaller_num:
+    #larger_num /= 2
+#    num += 1
+
+#print(f"number of times halved: {num} ")
+
+#question 2
+
+#word = input("enter a word: ")
+#result = ""
+#for i in range(1 , len(word) - 1,2):
+  #  result += word(i)
+    
+#print(f"output = {result}")
+
+#question 3
+
+num = int(Input("enter a number: "))
+for each num in range(37, 1050, +1):
+    if num %2==0:
+'''''
+#question 4
+word = ""
+
+while True:
+    user_in = input("enter the letter: ")
+    if user_in == "done":
+        break
+    else:
+        word += user_in
+print(f"the final word is {word}")
